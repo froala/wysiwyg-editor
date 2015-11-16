31ad8f7f14029223f11ccab83af5a8072ab34058
Stefan Neculai <stefan.neculai@gmail.com>
2015-11-16 19:22:35 +0200

    Improve Croatian translation.

--- js/languages/hr.js
+++ js/languages/hr.js
@@ -11,13 +11,13 @@
 $.FroalaEditor.LANGUAGE['hr'] = {
   translation: {
     // Place holder
-    "Type something": "Ukucaj ne\u0161to",
+    "Type something": "Napi\u0161i ne\u0161to",
 
     // Basic formatting
     "Bold": "Podebljaj",
     "Italic": "Kurziv",
-    "Underline": "Crta ispod",
-    "Strikethrough": "Crta kroz sredinu",
+    "Underline": "Podcrtano",
+    "Strikethrough": "Precrtano",
 
     // Main buttons
     "Insert": "Umetni",
@@ -25,9 +25,9 @@ $.FroalaEditor.LANGUAGE['hr'] = {
     "Cancel": "Otka\u017ei",
     "OK": "U redu",
     "Back": "Natrag",
-    "Remove": "Ukloniti",
+    "Remove": "Ukloni",
     "More": "Vi\u0161e",
-    "Update": "A\u017eurirati",
+    "Update": "A\u017euriraj",
     "Style": "Stil",
 
     // Font
@@ -36,21 +36,21 @@ $.FroalaEditor.LANGUAGE['hr'] = {
 
     // Colors
     "Colors": "Boje",
-    "Background": "Pozadine",
-    "Text": "Teksta",
+    "Background": "Pozadina",
+    "Text": "Tekst",
 
     // Paragraphs
-    "Paragraph Format": "Paragraf formatu",
+    "Paragraph Format": "Format odlomka",
     "Normal": "Normalno",
     "Code": "Izvorni kod",
-    "Heading 1": "Zaglavlje 1",
-    "Heading 2": "Zaglavlje 2",
-    "Heading 3": "Zaglavlje 3",
-    "Heading 4": "Zaglavlje 4",
+    "Heading 1": "Naslov 1",
+    "Heading 2": "Naslov 2",
+    "Heading 3": "Naslov 3",
+    "Heading 4": "Naslov 4",
 
     // Style
-    "Paragraph Style": "Paragraf stil",
-    "Inline Style": "U linija stil",
+    "Paragraph Style": "Stil odlomka",
+    "Inline Style": "Stil u liniji",
 
     // Alignment
     "Align": "Poravnaj",
@@ -61,12 +61,12 @@ $.FroalaEditor.LANGUAGE['hr'] = {
     "None": "Nijedan",
 
     // Lists
-    "Ordered List": "Lista naredio",
-    "Unordered List": "Neure\u0111en lista",
+    "Ordered List": "Ure\u0111ena lista",
+    "Unordered List": "Neure\u0111ena lista",
 
     // Indent
-    "Decrease Indent": "Uvuci paragraf",
-    "Increase Indent": "Izvuci paragraf",
+    "Decrease Indent": "Uvuci odlomak",
+    "Increase Indent": "Izvuci odlomak",
 
     // Links
     "Insert Link": "Umetni link",
@@ -74,31 +74,31 @@ $.FroalaEditor.LANGUAGE['hr'] = {
     "Open Link": "Otvori link",
     "Edit Link": "Uredi link",
     "Unlink": "Ukloni link",
-    "Choose Link": "Odaberite link",
+    "Choose Link": "Odaberi link",
 
     // Images
     "Insert Image": "Umetni sliku",
-    "Upload Image": "Postavi sliku",
+    "Upload Image": "Prijenos slike",
     "By URL": "Prema URL",
-    "Browse": "Brstiti",
-    "Drop image": "Izbaci sliku",
+    "Browse": "Odabir",
+    "Drop image": "Ispusti sliku",
     "or click": "ili odaberi",
-    "Manage Images": "Upravljanje slike",
-    "Loading": "Koji tovari",
+    "Manage Images": "Upravljanje slikama",
+    "Loading": "U\u010ditavanje",
     "Deleting": "Brisanje",
     "Tags": "Oznake",
     "Are you sure? Image will be deleted.": "Da li ste sigurni da \u017eelite obrisati ovu sliku?",
     "Replace": "Zamijeni",
     "Uploading": "Prijenos",
     "Loading image": "Otvaram sliku",
-    "Display": "Izlo\u017eiti",
-    "Inline": "Inline",
+    "Display": "Prika\u017ei",
+    "Inline": "U liniji",
     "Break Text": "Odvojeni tekst",
     "Alternate Text": "Alternativni tekst",
     "Change Size": "Promjena veli\u010dine",
     "Width": "\u0160irina",
     "Height": "Visina",
-    "Something went wrong. Please try again.": "Ne\u0161to je po\u0161lo po zlu. Molim te poku\u0161aj ponovno.",
+    "Something went wrong. Please try again.": "Ne\u0161to je po\u0161lo po zlu. Molimo poku\u0161ajte ponovno.",
 
     // Video
     "Insert Video": "Umetni video",
@@ -120,108 +120,110 @@ $.FroalaEditor.LANGUAGE['hr'] = {
     "Horizontal split": "Horizontalno razdvajanje polja",
     "Vertical split": "Vertikalno razdvajanje polja",
     "Cell Background": "Polje pozadine",
-    "Vertical Align": "Vertikalni poravnaj",
+    "Vertical Align": "Vertikalno poravnanje",
     "Top": "Vrh",
-    "Middle": "Srednji",
+    "Middle": "Sredina",
     "Bottom": "Dno",
-    "Align Top": "Poravnaj vrh",
-    "Align Middle": "Poravnaj srednji",
-    "Align Bottom": "Poravnaj dno",
-    "Cell Style": "Polje stil",
+    "Align Top": "Poravnaj na vrh",
+    "Align Middle": "Poravnaj po sredini",
+    "Align Bottom": "Poravnaj na dno",
+    "Cell Style": "Stil polja",
+    "Table Header": "Zaglavlje tablice",
+    "Remove Table": "Izbri\u0161i tablicu",
 
     // Files
     "Upload File": "Prijenos datoteke",
-    "Drop file": "Izbaci datoteke",
+    "Drop file": "Ispusti datoteku",
 
     // Emoticons
-    "Emoticons": "Emotikone",
-    "Grinning face": "Nasmijav\u0161i lice",
-    "Grinning face with smiling eyes": "Nasmijav\u0161i lice s nasmije\u0161enim o\u010dima",
+    "Emoticons": "Emotikoni",
+    "Grinning face": "Nacereno lice",
+    "Grinning face with smiling eyes": "Nacereno lice s nasmije\u0161enim o\u010dima",
     "Face with tears of joy": "Lice sa suzama radosnicama",
-    "Smiling face with open mouth": "Nasmijana lica s otvorenim ustima",
-    "Smiling face with open mouth and smiling eyes": "Nasmijana lica s otvorenim ustima i nasmijana o\u010di",
-    "Smiling face with open mouth and cold sweat": "Nasmijana lica s otvorenim ustima i hladan znoj",
-    "Smiling face with open mouth and tightly-closed eyes": "Nasmijana lica s otvorenim ustima i \u010dvrsto zatvorenih o\u010diju",
-    "Smiling face with halo": "Nasmijana lica sa halo",
-    "Smiling face with horns": "Nasmijana lica s rogovima",
-    "Winking face": "Namigivanje lice",
-    "Smiling face with smiling eyes": "Nasmijana lica s nasmiješenim o\u010dima",
-    "Face savoring delicious food": "Liceu\u017eivaju\u0107i ukusnu hranu",
-    "Relieved face": "S olak\u0161anjem lice",
-    "Smiling face with heart-shaped eyes": "Nasmijana lice u obliku srca o\u010di",
-    "Smiling face with sunglasses": "Nasmijana lica sa sun\u010danim nao\u010dalama",
-    "Smirking face": "Smije\u0161e\u0107i se lice",
+    "Smiling face with open mouth": "Nasmijano lice s otvorenim ustima",
+    "Smiling face with open mouth and smiling eyes": "Nasmijano lice s otvorenim ustima i nasmijanim o\u010dima",
+    "Smiling face with open mouth and cold sweat": "Nasmijano lice s otvorenim ustima i hladnim znojem",
+    "Smiling face with open mouth and tightly-closed eyes": "Nasmijano lice s otvorenim ustima i \u010dvrsto zatvorenih o\u010diju",
+    "Smiling face with halo": "Nasmijano lice sa aureolom",
+    "Smiling face with horns": "Nasmijano lice s rogovima",
+    "Winking face": "Lice koje namiguje",
+    "Smiling face with smiling eyes": "Nasmijano lice s nasmiješenim o\u010dima",
+    "Face savoring delicious food": "Lice koje u\u017eiva ukusnu hranu",
+    "Relieved face": "Lice s olak\u0161anjem",
+    "Smiling face with heart-shaped eyes": "Nasmijano lice sa o\u010dima u obliku srca",
+    "Smiling face with sunglasses": "Nasmijano lice sa sun\u010danim nao\u010dalama",
+    "Smirking face": "Zlokobno nasmije\u0161eno lice",
     "Neutral face": "Neutralno lice",
     "Expressionless face": "Bezizra\u017eajno lice",
-    "Unamused face": "Ne zabavlja lice",
+    "Unamused face": "Nezainteresirano lice",
     "Face with cold sweat": "Lice s hladnim znojem",
     "Pensive face": "Zami\u0161ljeno lice",
-    "Confused face": "Zbunjen lice",
-    "Confounded face": "Smesti lice",
-    "Kissing face": "Ljubljenje lice",
-    "Face throwing a kiss": "Lice bacanje poljubac",
-    "Kissing face with smiling eyes": "Ljubljenje lice s nasmije\u0161enim o\u010dima",
-    "Kissing face with closed eyes": "Ljubljenje lice zatvorenih o\u010diju",
-    "Face with stuck out tongue": "Lice s ispru\u017eio jezik",
-    "Face with stuck out tongue and winking eye": "Lice s ispru\u017eio jezik i namigivanje okom",
-    "Face with stuck out tongue and tightly-closed eyes": "Lice s ispru\u017eio jezik i \u010dvrsto zatvorenih o\u010diju",
-    "Disappointed face": "Razo\u010daran lice",
-    "Worried face": "Zabrinuti lice",
-    "Angry face": "Ljut lice",
-    "Pouting face": "Ispu\u010dene lice",
-    "Crying face": "Pla\u010de lice",
-    "Persevering face": "Ula\u017eu\u0107i lice",
-    "Face with look of triumph": "Lice s izrazom trijumf",
-    "Disappointed but relieved face": "Razo\u010daran ali laknulo lice",
-    "Frowning face with open mouth": "Mr\u0161te\u0107i se lice s otvorenim ustima",
+    "Confused face": "Zbunjeno lice",
+    "Confounded face": "Zbunjeno lice",
+    "Kissing face": "Lice s poljupcem",
+    "Face throwing a kiss": "Lice koje baca poljubac",
+    "Kissing face with smiling eyes": "Lice s poljupcem s nasmije\u0161enim o\u010dima",
+    "Kissing face with closed eyes": "Lice s poljupcem zatvorenih o\u010diju",
+    "Face with stuck out tongue": "Lice s ispru\u017eenim jezikom",
+    "Face with stuck out tongue and winking eye": "Lice s ispru\u017eenim jezikom koje namiguje",
+    "Face with stuck out tongue and tightly-closed eyes": "Lice s ispru\u017eenim jezikom i \u010dvrsto zatvorenih o\u010diju",
+    "Disappointed face": "Razo\u010darano lice",
+    "Worried face": "Zabrinuto lice",
+    "Angry face": "Ljutito lice",
+    "Pouting face": "Nadureno lice",
+    "Crying face": "Uplakano lice",
+    "Persevering face": "Lice s negodovanjem",
+    "Face with look of triumph": "Trijumfalno lice",
+    "Disappointed but relieved face": "Razo\u010darano ali olakšano lice",
+    "Frowning face with open mouth": "Namrgo\u0111eno lice s otvorenim ustima",
     "Anguished face": "Tjeskobno lice",
-    "Fearful face": "U strahu lice",
+    "Fearful face": "Prestra\u0161eno lice",
     "Weary face": "Umorno lice",
     "Sleepy face": "Pospano lice",
-    "Tired face": "Umoran lice",
-    "Grimacing face": "Grimase lice",
-    "Loudly crying face": "Glasno pla\u010du\u0107i lice",
+    "Tired face": "Umorno lice",
+    "Grimacing face": "Lice sa grimasama",
+    "Loudly crying face": "Glasno pla\u010du\u0107e lice",
     "Face with open mouth": "Lice s otvorenim ustima",
     "Hushed face": "Tiho lice",
-    "Face with open mouth and cold sweat": "Lice s otvorenim ustima i hladan znoj",
-    "Face screaming in fear": "Lice vri\u0161ti u strahu",
-    "Astonished face": "Zaprepa\u0161ten lice",
+    "Face with open mouth and cold sweat": "Lice s otvorenim ustima i hladnim znojem",
+    "Face screaming in fear": "Lice koje vri\u0161ti u strahu",
+    "Astonished face": "Zaprepa\u0161teno lice",
     "Flushed face": "Zajapureno lice",
-    "Sleeping face": "Spava\u0107a lica",
-    "Dizzy face": "Vrtoglavicu lica",
+    "Sleeping face": "Spava\u0107e lice",
+    "Dizzy face": "Lice sa vrtoglavicom",
     "Face without mouth": "Lice bez usta",
     "Face with medical mask": "Lice s medicinskom maskom",
 
     // Line breaker
-    "Break": "Odvojeni",
+    "Break": "Odvojeno",
 
     // Math
     "Subscript": "Indeks",
     "Superscript": "Eksponent",
 
     // Full screen
-    "Fullscreen": "Preko cijelog zaslona",
+    "Fullscreen": "Puni zaslon",
 
     // Horizontal line
     "Insert Horizontal Line": "Umetni liniju",
 
     // Clear formatting
-    "Clear Formatting": "Ukloniti oblikovanje",
+    "Clear Formatting": "Ukloni oblikovanje",
 
     // Undo, redo
     "Undo": "Korak natrag",
     "Redo": "Korak naprijed",
 
     // Select all
-    "Select All": "Sa\u010duvaj sve",
+    "Select All": "Odaberi sve",
 
     // Code view
-    "Code View": "Pogled kod",
+    "Code View": "Pregled koda",
 
     // Quote
     "Quote": "Citat",
-    "Increase": "Pove\u0107ati",
-    "Decrease": "Smanjenje"
+    "Increase": "Pove\u0107aj",
+    "Decrease": "Smanji"
   },
   direction: "ltr"
 };
