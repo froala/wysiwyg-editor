337f811380f7b9317487b8ac47f0aef5de73e7dc
Stefan Neculai <stefan.neculai@gmail.com>
2015-11-16 11:13:33 +0200

    Clean HTML examples.

--- html/file_s3_upload.php
+++ html/file_s3_upload.php
@@ -130,29 +130,30 @@
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.3.0/codemirror.min.js"></script>
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.3.0/mode/xml/xml.min.js"></script>
-  <script src="../js/froala_editor.min.js"></script>
-  <script src="../js/plugins/align.min.js"></script>
-  <script src="../js/plugins/code_view.min.js"></script>
-  <script src="../js/plugins/colors.min.js"></script>
-  <script src="../js/plugins/emoticons.min.js"></script>
-  <script src="../js/plugins/font_size.min.js"></script>
-  <script src="../js/plugins/font_family.min.js"></script>
-  <script src="../js/plugins/image.min.js"></script>
-  <script src="../js/plugins/image_manager.min.js"></script>
-  <script src="../js/plugins/line_breaker.min.js"></script>
-  <script src="../js/plugins/link.min.js"></script>
-  <script src="../js/plugins/lists.min.js"></script>
-  <script src="../js/plugins/paragraph_format.min.js"></script>
-  <script src="../js/plugins/paragraph_style.min.js"></script>
-  <script src="../js/plugins/video.min.js"></script>
-  <script src="../js/plugins/table.min.js"></script>
-  <script src="../js/plugins/url.min.js"></script>
-  <script src="../js/plugins/entities.min.js"></script>
-  <script src="../js/plugins/char_counter.min.js"></script>
-  <script src="../js/plugins/inline_style.min.js"></script>
-  <script src="../js/plugins/save.min.js"></script>
-  <script src="../js/plugins/file.min.js"></script>
-  <script src="../js/languages/ro.js"></script>
+  <script type="text/javascript" src="../js/froala_editor.min.js"></script>
+  <script type="text/javascript" src="../js/plugins/align.min.js"></script>
+  <script type="text/javascript" src="../js/plugins/code_beautifier.min.js"></script>
+  <script type="text/javascript" src="../js/plugins/code_view.min.js"></script>
+  <script type="text/javascript" src="../js/plugins/colors.min.js"></script>
+  <script type="text/javascript" src="../js/plugins/emoticons.min.js"></script>
+  <script type="text/javascript" src="../js/plugins/font_size.min.js"></script>
+  <script type="text/javascript" src="../js/plugins/font_family.min.js"></script>
+  <script type="text/javascript" src="../js/plugins/image.min.js"></script>
+  <script type="text/javascript" src="../js/plugins/image_manager.min.js"></script>
+  <script type="text/javascript" src="../js/plugins/line_breaker.min.js"></script>
+  <script type="text/javascript" src="../js/plugins/link.min.js"></script>
+  <script type="text/javascript" src="../js/plugins/lists.min.js"></script>
+  <script type="text/javascript" src="../js/plugins/paragraph_format.min.js"></script>
+  <script type="text/javascript" src="../js/plugins/paragraph_style.min.js"></script>
+  <script type="text/javascript" src="../js/plugins/video.min.js"></script>
+  <script type="text/javascript" src="../js/plugins/table.min.js"></script>
+  <script type="text/javascript" src="../js/plugins/url.min.js"></script>
+  <script type="text/javascript" src="../js/plugins/entities.min.js"></script>
+  <script type="text/javascript" src="../js/plugins/char_counter.min.js"></script>
+  <script type="text/javascript" src="../js/plugins/inline_style.min.js"></script>
+  <script type="text/javascript" src="../js/plugins/save.min.js"></script>
+  <script type="text/javascript" src="../js/plugins/file.min.js"></script>
+  <script type="text/javascript" src="../js/languages/ro.js"></script>
 
   <script>
     $(function(){
