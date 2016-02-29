555b44fa76864b589e38a057c38ee817fe70cbf6
Stefan Neculai <stefan.neculai@gmail.com>
2016-02-29 21:05:33 +0200

    Start V2.3.0.

--- css/plugins/code_view.min.css
+++ css/plugins/code_view.min.css
@@ -1,6 +1,6 @@
 /*!
  * froala_editor v2.3.0 (https://www.froala.com/wysiwyg-editor)
- * License https://froala.com/wysiwyg-editor/terms
+ * License https://froala.com/wysiwyg-editor/terms/
  * Copyright 2014-2016 Froala Labs
  */
 
