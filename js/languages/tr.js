5b940c321f83472563778b4d9caa67d52f2036aa
Stefan Neculai <stefan.neculai@gmail.com>
2017-05-22 12:16:01 +0000

    Fixes email detection in URL typing.

--- js/languages/tr.js
+++ js/languages/tr.js
@@ -4,6 +4,32 @@
  * Copyright 2014-2017 Froala Labs
  */
 
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jquery'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node/CommonJS
+        module.exports = function( root, jQuery ) {
+            if ( jQuery === undefined ) {
+                // require('jQuery') returns a factory that requires window to
+                // build a jQuery instance, we normalize how we use modules
+                // that require this pattern but the window provided is a noop
+                // if it's defined (how jquery works)
+                if ( typeof window !== 'undefined' ) {
+                    jQuery = require('jquery');
+                }
+                else {
+                    jQuery = require('jquery')(root);
+                }
+            }
+            return factory(jQuery);
+        };
+    } else {
+        // Browser globals
+        factory(window.jQuery);
+    }
+}(function ($) {
 /**
  * Turkish
  */
@@ -231,3 +257,5 @@ $.FE.LANGUAGE['tr'] = {
   },
   direction: "ltr"
 };
+
+}));
