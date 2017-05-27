03ed03645b07a72fc39697bca20d823f44b50b5f
Stefan Neculai <stefan.neculai@gmail.com>
2017-05-27 13:35:43 +0000

    Start 2.6.2.

--- css/plugins/special_characters.min.css
+++ css/plugins/special_characters.min.css
@@ -1,5 +1,5 @@
 /*!
- * froala_editor v2.6.1 (https://www.froala.com/wysiwyg-editor)
+ * froala_editor v2.6.2 (https://www.froala.com/wysiwyg-editor)
  * License https://froala.com/wysiwyg-editor/terms/
  * Copyright 2014-2017 Froala Labs
  */
