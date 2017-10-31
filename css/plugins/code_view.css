a94a73ba21f0aad8a347a641395e5c85b0835f33
Stefan Neculai <stefan.neculai@gmail.com>
2017-10-31 12:37:07 +0000

    Start v2.7.2.

--- css/plugins/code_view.css
+++ css/plugins/code_view.css
@@ -1,5 +1,5 @@
 /*!
- * froala_editor v2.7.1 (https://www.froala.com/wysiwyg-editor)
+ * froala_editor v2.7.2 (https://www.froala.com/wysiwyg-editor)
  * License https://froala.com/wysiwyg-editor/terms/
  * Copyright 2014-2017 Froala Labs
  */
