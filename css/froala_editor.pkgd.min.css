28895453f23fb492e213c9109740a38f6766d3fd
Stefan Neculai <stefan.neculai@gmail.com>
2018-04-22 12:41:02 +0000

    Start v2.8.1.

--- css/froala_editor.pkgd.min.css
+++ css/froala_editor.pkgd.min.css
@@ -1,5 +1,5 @@
 /*!
- * froala_editor v2.8.0 (https://www.froala.com/wysiwyg-editor)
+ * froala_editor v2.8.1 (https://www.froala.com/wysiwyg-editor)
  * License https://froala.com/wysiwyg-editor/terms/
  * Copyright 2014-2018 Froala Labs
  */
