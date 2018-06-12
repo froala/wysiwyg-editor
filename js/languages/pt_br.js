ebe0eb93275611a09a76c611b7165b35c2d5c05c
Giovani Generali <giovani.generali@gmail.com>
2018-06-12 17:58:41 -0300

    improvement

--- js/languages/pt_br.js
+++ js/languages/pt_br.js
@@ -22,9 +22,9 @@
 
       // Basic formatting
       "Bold": "Negrito",
-      "Italic": 'It\xE1lico',
+      "Italic": 'Itálito',
       "Underline": "Sublinhar",
-      "Strikethrough": "Riscar",
+      "Strikethrough": "Tachado",
 
       // Main buttons
       "Insert": "Inserir",
@@ -50,27 +50,27 @@
       // Paragraphs
       "Paragraph Format": "Formatos",
       "Normal": "Normal",
-      "Code": 'C\xF3digo',
-      "Heading 1": 'Cabe\xE7alho 1',
-      "Heading 2": 'Cabe\xE7alho 2',
-      "Heading 3": 'Cabe\xE7alho 3',
-      "Heading 4": 'Cabe\xE7alho 4',
+      "Code": 'Código',
+      "Heading 1": 'Cabeçalho 1',
+      "Heading 2": 'Cabeçalho 2',
+      "Heading 3": 'Cabeçalho 3',
+      "Heading 4": 'Cabeçalho 4',
 
       // Style
-      "Paragraph Style": 'Estilo de par\xE1grafo',
+      "Paragraph Style": 'Estilo de parágrafo',
       "Inline Style": "Estilo embutido",
 
       // Alignment
       "Align": "Alinhar",
-      "Align Left": 'Alinhar \xE0 esquerda',
+      "Align Left": 'Alinhar à esquerda',
       "Align Center": "Centralizar",
-      "Align Right": 'Alinhar \xE0 direita',
+      "Align Right": 'Alinhar à direita',
       "Align Justify": "Justificar",
       "None": "Nenhum",
 
       // Lists
       "Ordered List": "Lista ordenada",
-      "Unordered List": 'Lista n\xE3o ordenada',
+      "Unordered List": 'Lista não ordenada',
 
       // Indent
       "Decrease Indent": "Diminuir recuo",
@@ -87,7 +87,7 @@
       // Images
       "Insert Image": "Inserir imagem",
       "Upload Image": "Carregar imagem",
-      "By URL": "Por URL",
+      "By URL": "Por um endereço URL",
       "Browse": "Procurar",
       "Drop image": "Arraste sua imagem aqui",
       "or click": "ou clique aqui",
@@ -95,7 +95,7 @@
       "Loading": "Carregando",
       "Deleting": "Excluindo",
       "Tags": "Etiquetas",
-      "Are you sure? Image will be deleted.": 'Voc\xEA tem certeza? Imagem ser\xE1 apagada.',
+      "Are you sure? Image will be deleted.": 'Você tem certeza? A imagem será apagada.',
       "Replace": "Substituir",
       "Uploading": "Carregando imagem",
       "Loading image": "Carregando imagem",
@@ -111,16 +111,16 @@
       "Advanced Edit": "Edição avançada",
 
       // Video
-      "Insert Video": 'Inserir v\xEDdeo',
-      "Embedded Code": 'C\xF3digo embutido',
-      "Paste in a video URL": "Colar em um URL de vídeo",
-      "Drop video": "Solte o video",
-      "Your browser does not support HTML5 video.": "Seu navegador não suporta o vídeo html5.",
-      "Upload Video": "Envio vídeo",
+      "Insert Video": 'Inserir vídeo',
+      "Embedded Code": 'Código embutido',
+      "Paste in a vídeo URL": "Colar um endereço de vídeo",
+      "Drop video": "Solte o vídeo",
+      "Your browser does not support HTML5 vídeo.": "Seu navegador não suporta vídeo em HTML5.",
+      "Upload Video": "Carregar vídeo",
 
       // Tables
       "Insert Table": "Inserir tabela",
-      "Table Header": 'Cabe\xE7alho da tabela',
+      "Table Header": 'Cabeçalho da tabela',
       "Remove Table": "Remover tabela",
       "Table Style": "estilo de tabela",
       "Horizontal Align": "Alinhamento horizontal",
@@ -132,11 +132,11 @@
       "Insert column before": "Inserir coluna antes",
       "Insert column after": "Inserir coluna depois",
       "Delete column": "Excluir coluna",
-      "Cell": 'C\xE9lula',
-      "Merge cells": 'Agrupar c\xE9lulas',
-      "Horizontal split": 'Divis\xE3o horizontal',
-      "Vertical split": 'Divis\xE3o vertical',
-      "Cell Background": 'Fundo da c\xE9lula',
+      "Cell": 'Célula',
+      "Merge cells": 'Agrupar células',
+      "Horizontal split": 'Divisão horizontal',
+      "Vertical split": 'Divisão vertical',
+      "Cell Background": 'Fundo da célula',
       "Vertical Align": "Alinhamento vertical",
       "Top": "Topo",
       "Middle": "Meio",
@@ -144,17 +144,17 @@
       "Align Top": "Alinhar topo",
       "Align Middle": "Alinhar meio",
       "Align Bottom": "Alinhar fundo",
-      "Cell Style": 'Estilo de c\xE9lula',
+      "Cell Style": 'Estilo de célula',
 
       // Files
-      "Upload File": "Upload de arquivo",
+      "Upload File": "Carregar arquivo",
       "Drop file": "Arraste seu arquivo aqui",
 
       // Emoticons
       "Emoticons": "Emoticons",
       "Grinning face": "Sorrindo a cara",
       "Grinning face with smiling eyes": "Sorrindo rosto com olhos sorridentes",
-      "Face with tears of joy": 'Rosto com l\xE1grimas de alegria',
+      "Face with tears of joy": 'Rosto com lágrimas de alegria',
       "Smiling face with open mouth": "Rosto de sorriso com a boca aberta",
       "Smiling face with open mouth and smiling eyes": "Rosto de sorriso com a boca aberta e olhos sorridentes",
       "Smiling face with open mouth and cold sweat": "Rosto de sorriso com a boca aberta e suor frio",
@@ -165,22 +165,22 @@
       "Smiling face with smiling eyes": "Rosto de sorriso com olhos sorridentes",
       "Face savoring delicious food": "Rosto saboreando uma deliciosa comida",
       "Relieved face": "Rosto aliviado",
-      "Smiling face with heart-shaped eyes": 'Rosto de sorriso com os olhos em forma de cora\xE7\xE3o',
-      "Smiling face with sunglasses": 'Rosto de sorriso com \xF3culos de sol',
+      "Smiling face with heart-shaped eyes": 'Rosto de sorriso com os olhos em forma de coração',
+      "Smiling face with sunglasses": 'Rosto de sorriso com óculos de sol',
       "Smirking face": "Rosto sorridente",
       "Neutral face": "Rosto neutra",
       "Expressionless face": "Rosto inexpressivo",
-      "Unamused face": 'O rosto n\xE3o divertido',
+      "Unamused face": 'O rosto não divertido',
       "Face with cold sweat": "Rosto com suor frio",
       "Pensive face": "O rosto pensativo",
       "Confused face": "Cara confusa",
-      "Confounded face": 'Rosto at\xF4nito',
+      "Confounded face": 'Rosto atônito',
       "Kissing face": "Beijar Rosto",
       "Face throwing a kiss": "Rosto jogando um beijo",
       "Kissing face with smiling eyes": "Beijar rosto com olhos sorridentes",
       "Kissing face with closed eyes": "Beijando a cara com os olhos fechados",
-      "Face with stuck out tongue": 'Preso de cara com a l\xEDngua para fora',
-      "Face with stuck out tongue and winking eye": 'Rosto com estendeu a l\xEDngua e olho piscando',
+      "Face with stuck out tongue": 'Preso de cara com a língua para fora',
+      "Face with stuck out tongue and winking eye": 'Rosto com estendeu a língua e olho piscando',
       "Face with stuck out tongue and tightly-closed eyes": "Rosto com estendeu a língua e os olhos bem fechados",
       "Disappointed face": "Rosto decepcionado",
       "Worried face": "O rosto preocupado",
@@ -207,10 +207,10 @@
       "Sleeping face": "O rosto de sono",
       "Dizzy face": "Cara tonto",
       "Face without mouth": "Rosto sem boca",
-      "Face with medical mask": 'Rosto com m\xE1scara m\xE9dica',
+      "Face with medical mask": 'Rosto com máscara médica',
 
       // Line breaker
-      "Break": "Quebrar",
+      "Break": "Quebrar linha",
 
       // Math
       "Subscript": "Subscrito",
@@ -223,7 +223,7 @@
       "Insert Horizontal Line": "Inserir linha horizontal",
 
       // Clear formatting
-      "Clear Formatting": 'Remover formata\xE7\xE3o',
+      "Clear Formatting": 'Remover formatação',
 
       // Undo, redo
       "Undo": "Desfazer",
@@ -233,15 +233,15 @@
       "Select All": "Selecionar tudo",
 
       // Code view
-      "Code View": 'Exibi\xE7\xE3o de c\xF3digo',
+      "Code View": 'Exibir código-fonte',
 
       // Quote
-      "Quote": 'Cita\xE7\xE3o',
+      "Quote": 'Citação',
       "Increase": "Aumentar",
       "Decrease": "Diminuir",
 
       // Quick Insert
-      "Quick Insert": 'Inser\xE7\xE3o r\xE1pida',
+      "Quick Insert": 'Inserção rápida',
 
       // Spcial Characters
       "Special Characters": "Caracteres especiais",
@@ -250,7 +250,7 @@
       "Cyrillic": "Cirílico",
       "Punctuation": "Pontuação",
       "Currency": "Moeda",
-      "Arrows": "Setas; flechas",
+      "Arrows": "Setas",
       "Math": "Matemática",
       "Misc": "Misc",
 
@@ -261,7 +261,7 @@
       "Spell Checker": "Verificador ortográfico",
 
       // Help
-      "Help": "Socorro",
+      "Help": "Ajuda",
       "Shortcuts": "Atalhos",
       "Inline Editor": "Editor em linha",
       "Show the editor": "Mostre o editor",
@@ -272,7 +272,7 @@
       "Basic Formatting": "Formatação básica",
       "Increase quote level": "Aumentar o nível de cotação",
       "Decrease quote level": "Diminuir o nível de cotação",
-      "Image / Video": "Imagem / video",
+      "Image / Video": "Imagem / Vídeo",
       "Resize larger": "Redimensionar maior",
       "Resize smaller": "Redimensionar menor",
       "Table": "Tabela",
@@ -285,7 +285,7 @@
 
       // Embed.ly
       "Embed URL": "URL de inserção",
-      "Paste in a URL to embed": "Colar em url para incorporar",
+      "Paste in a URL to embed": "Colar um endereço URL para incorporar",
 
       // Word Paste.
       "The pasted content is coming from a Microsoft Word document. Do you want to keep the format or clean it up?": "O conteúdo colado vem de um documento Microsoft Word. Você quer manter o formato ou limpá-lo?",
