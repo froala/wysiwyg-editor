edd86c4d2e3b68cb9aad33e87a4d44e2700d6156
Kapil Garg <kapil.garg.techy@gmail.com>
2021-05-18 15:26:27 +0530

    Release 3.2.7

--- js/plugins/line_height.min.js
+++ js/plugins/line_height.min.js
@@ -1,5 +1,5 @@
 /*!
- * froala_editor v3.2.6-1 (https://www.froala.com/wysiwyg-editor)
+ * froala_editor v3.2.7 (https://www.froala.com/wysiwyg-editor)
  * License https://froala.com/wysiwyg-editor/terms/
  * Copyright 2014-2021 Froala Labs
  */
